ed4c93053c7791afebafce508a376cde91fd245a
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-12-17 15:23:46 -0500

    Working with Props

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LoginForm from "./src/components/LogingForm";
+import Greeting from "./src/components/Greeting";
 
 export default function App() {
 	return (
 		// View fundemental container on React Native
 		<View style={styles.container}>
-			<Text>First component</Text>
+			<Text>React Native course</Text>
+			<Greeting name="Jose Juaquin" />
+			<Greeting name="David Smith" />
+			<Greeting name="Marcelo Di Maria" />
+
 			<StatusBar style="auto" />
-			<LoginForm />
+			{/* <LoginForm /> */}
 		</View>
 	);
 }
